581cf9db18f86bf261fba63704cad1c34fbdc2f7
isaachaywork <isaachaywork@gmail.com>
2025-11-01 15:51:48 -0400

    Update features.py

--- src/features.py
+++ src/features.py
@@ -1,1 +1,13 @@
+import joblib
+import pandas as pd
+from sklearn,feature_extraction.text import TfidfVectorizer
 
+def fit_tfidf(train_path = 'data/train.parquet'):
+  df = pd.read_parquet(train_path)
+  vec = TfidfVectorizer(max_features=20000, ngram_range(1,2))
+  X = vec.fit_transform(df['text_clean'])
+  joblib.dump(vec, 'models/tfidf_vectorizer.joblib')
+  print('Saved TF - IDF vectorizer')
+
+if __name__ == '__main__':
+  fit_tfidf()
